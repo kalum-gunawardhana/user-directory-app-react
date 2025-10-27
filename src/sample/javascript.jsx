56b4944cfd12f41e5778919765474c001e286086
Kalum Gunawardhana <kguna415@gmail.com>
2025-10-27 21:32:55 +0530

    commit

---
+++ src/sample/javascript.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const element = <h1>Hello, World!</h1>;
+
+const element1 = React.createElement('h1', null, 'Hello, World!');
+
+return (
+    <div>
+        <h1>Hello, World!</h1>
+        <p>This is a simple React component.</p>
+    </div>
+);
+
+export default javascript
