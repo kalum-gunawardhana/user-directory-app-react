1d0b06b4bb647fed59755c4f8688f824d3a7e4e2
Kalum Gunawardhana <kguna415@gmail.com>
2025-10-27 23:23:26 +0530

    commit

---
+++ src/sample/useEffect.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+
+function UserList() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+            .catch(err => console.error(err));
+    }, []);
+
+    return (
+        <div>
+            <h2>User List</h2>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
