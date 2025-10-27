1a6579a3f8d54387a618155e2cb0874077eb91e2
Kalum Gunawardhana <kguna415@gmail.com>
2025-10-27 21:19:58 +0530

    commit

---
+++ src/sample/welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function welcome() {
+    return <div>Welcome to the User Directory</div>
+}
+
+const welcome = () => {
+    return <div>Welcome to the User Directory</div>
+}
+
+export default welcome
